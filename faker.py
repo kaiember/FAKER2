2424d112d940265ab39b296b1429bc273d830d4e
kaiember <dayomusiliu980@gmail.com>
2026-02-27 05:55:15 +0100

    fixed an issue

---
+++ faker.py
@@ -0,0 +1,12 @@
+from faker import Faker
+import os
+fake = Faker()
+os.system('clear')
+for y in range(5):
+    print(f" Name: {fake.name()}")
+    print(f" Email: {fake.email()}")
+    print(f" phone number: {fake.phone_number()}")
+    print(f" address: {fake.address()}")
+    print(f" text: {fake.text()}")
+    print(f" ="*30)
+    
